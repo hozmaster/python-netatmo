1077c83b885a246baa99af80c7df1f06d03d0e91
Olli-Pekka Wallin <opwallin@gmail.com>
2018-02-10 23:16:47 +0200

    Add file existence check, other chanegs

--- meteo.py
+++ meteo.py
@@ -2,48 +2,51 @@
 import requests
 from pprint import pprint
 import json
+import os.path
 
-data = json.load(open('config.json'))
+json_file = 'config.json'
+data = None
+if os.path.isfile(json_file)==True:
+    data = json.load(open(json_file))
 
+    if data is not None:
+        pprint(data)
 
-if data is not None:
-    pprint(data)
+        username = data['credentials']['username']
+        password = data['credentials']['password']
 
-    username = data['credentials']['username']
-    print(username)
+        netatmo = data['netatmo']
 
-    password = data['credentials']['password']
-    print(password)
-
-    netatmo = data['netatmo']
-
-    payload = {'grant_type': 'password',
+        payload = {'grant_type': 'password',
                'username': username,
                'password': password,
                'client_id': netatmo['api']['client_id'],
                'client_secret': netatmo['api']['client_secret'],
                'scope': 'read_station'}
 
-    print(payload)
+        print(payload)
 
-    response = requests.post("https://api.netatmo.com/oauth2/token", data=payload)
-    # response.raise_for_status()
-    # access_token=response.json()["access_token"]
-    # refresh_token=response.json()["refresh_token"]
-    # scope=response.json()["scope"]
-    #
-    # print("Your access token is:", access_token)
-    # print("Your refresh token is:", refresh_token)
-    # print("Your scopes are:", scope)
-    #
-    # params = {
-    #     'access_token': access_token,
-    #     'device_id': '70:ee:XX:XX:XX:XX'
-    # }
-    # response = requests.post("https://api.netatmo.com/api/getstationsdata", params=params)
-    # response.raise_for_status()
-    # data = response.json()["body"]
-    # print (data)
+        response = requests.post("https://api.netatmo.com/oauth2/token", data=payload)
 
-# except requests.exceptions.HTTPError as error:
-#     print(error.response.status_code, error.response.text)
+        # response.raise_for_status()
+        # access_token=response.json()["access_token"]
+        # refresh_token=response.json()["refresh_token"]
+        # scope=response.json()["scope"]
+        #
+        # print("Your access token is:", access_token)
+        # print("Your refresh token is:", refresh_token)
+        # print("Your scopes are:", scope)
+        #
+        # params = {
+        #     'access_token': access_token,
+        #     'device_id': '70:ee:XX:XX:XX:XX'
+        # }
+        # response = requests.post("https://api.netatmo.com/api/getstationsdata", params=params)
+        # response.raise_for_status()
+        # data = response.json()["body"]
+        # print (data)
+
+    # except requests.exceptions.HTTPError as error:
+    #     print(error.response.status_code, error.response.text)
+else:
+    print ('json file is not exists. Create it and try again.')
